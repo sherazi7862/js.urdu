2f3541e16377bb8f37f432cab7965a5bfa8c274f
sherazi7862 <155793950+sherazi7862@users.noreply.github.com>
2024-07-10 22:48:29 -0700

    Delete 0_basics directory

--- 0_basics/ 01_variable.js
+++
@@ -1,5 +0,0 @@
-const account_id =7862
-let accountemail="shahidsherazi761@gmail.com"
-var accountpassward="7862"
-console.log(account_id);
-console.table([account_id,accountemail,accountpassward]);
